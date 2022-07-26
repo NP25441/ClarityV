0d5d99fe4139a784f9bcb88e91ad3a2388e0fc13
Nathapat Prompasit <plwe1890@gmail.com>
2022-07-27 02:13:25 +0700

    Model_V_2

--- test_model.py
+++
@@ -1,9 +0,0 @@
-from keras.models import load_model
-
-model = load_model('model_car')
-model.summary()
-print("Loaded Model from disk")
-
-# compile and evaluate loaded model
-
-print(model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy']))
